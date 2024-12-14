032bd61356f773bea24d5f614c05a261e2429f33
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-12-14 12:24:09 +0100

    feat: nuovo array dinamico con componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// Importo l'array
+import posts from './data';
+
 // Importo il file di stile CSS
 import './App.css'
 
@@ -9,10 +12,13 @@ import AppMain from './components/AppMain'
 // Funzione che unisce i vari componenti in pagina
 function App() {
 
+// Array di elementi passando le proprietà dell'oggetto richiesto alla funzione
+const arrayPostJsx = posts.map((curPost) => <AppMain img={curPost.image} title={curPost.title} description={curPost.content}/>)
+
   return (
     <>
       <AppHeader></AppHeader>
-      <AppMain></AppMain>
+      {arrayPostJsx}
       <AppFooter></AppFooter>
     </>
   )
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,17 +1,17 @@
 import style from "./AppMain.module.css"
 
-function AppMain(){
+function AppMain({img, title, description, tags}){
     return (
         // Sezione della card intera del main
         <section className={style.card}>
 
             {/* Immgaine lotr*/}
-            <img src="./lotr_img.jpg" alt="" />
+            <img src={img} alt="" />
 
             {/* Container main testo */}
             <div className="textContainer">
-                <h5>La compagnia dell'anello</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quas maiores minus expedita odit suscipit consectetur esse ducimus, eos quos voluptatem quod iste magni voluptatibus atque adipisci porro praesentium. Iure.</p>
+                <h3>{title}</h3>
+                <p>{description}</p>
                 <button>LEGGI DI PIU'</button>
             </div>
 
